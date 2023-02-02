b246fb699e5c95a853fa617b5c0f1e7380655141
schorio <schorioignace@gmail.com>
2023-02-02 08:40:24 +0300

    mise en place de LogoutServlet

--- src/main/java/cn/ecommerce/servlet/LogoutServlet.java
+++ src/main/java/cn/ecommerce/servlet/LogoutServlet.java
@@ -1,6 +1,8 @@
 package cn.ecommerce.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +17,15 @@ public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		try(PrintWriter out = response.getWriter()){
+			if(request.getSession().getAttribute("auth") != null) {
+				request.getSession().removeAttribute("auth");
+				response.sendRedirect("login.jsp");
+			}
+			else {
+				response.sendRedirect("index.jsp");
+			}
+		}
 	}
 
 }
