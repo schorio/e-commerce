6edf8a9803425579d84419ff1b845fa2d68e71f8
schorio <schorioignace@gmail.com>
2023-02-02 07:23:59 +0300

    mise en neutre du class LoginServlet

--- src/main/java/cn/ecommerce/servlet/LoginServler.java
+++ src/main/java/cn/ecommerce/servlet/LoginServler.java
@@ -15,16 +15,12 @@ public class LoginServler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
