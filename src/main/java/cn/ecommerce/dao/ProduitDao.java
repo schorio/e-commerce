8b28678d5f42ea2a63ccb40bee42acc5f01c56fa
schorio <schorioignace@gmail.com>
2023-02-02 10:19:46 +0300

    creation du List contenant les produits de la BD

--- src/main/java/cn/ecommerce/dao/ProduitDao.java
+++ src/main/java/cn/ecommerce/dao/ProduitDao.java
@@ -3,6 +3,8 @@ package cn.ecommerce.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.*;
+import cn.ecommerce.model.*;
 
 public class ProduitDao {
 	private Connection con;
@@ -14,4 +16,30 @@ public class ProduitDao {
 		this.con = con;
 	}
 	
+	
+	public List<Produit> getAllProduit(){
+		List<Produit> produits = new ArrayList<Produit>();
+		
+		try {
+			query = "select * from produits";
+			pst = this.con.prepareStatement(query);
+			rs = pst.executeQuery();
+			
+			while(rs.next()) {
+				Produit row = new Produit();
+				row.setId_prd(rs.getInt("id_prd"));
+				row.setPrix_prd(rs.getInt("prix_prd"));
+				row.setNom_prd(rs.getString("nom_prd"));
+				row.setCategorie_prd(rs.getString("categorie_prd"));
+				row.setImage_prd(rs.getString("Image_prd"));
+				
+				produits.add(row);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return produits;
+	}
 }
