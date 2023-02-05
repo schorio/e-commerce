b2bd670f71ff816f057fe894a68902daa808b271
schorio <schorioignace@gmail.com>
2023-02-05 06:49:59 +0300

    Creation du modele Panier

---
+++ src/main/java/cn/ecommerce/model/Panier.java
@@ -0,0 +1,17 @@
+package cn.ecommerce.model;
+
+public class Panier extends Produit {
+	private int quantite;
+	
+	public Panier() { }
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+	
+}
